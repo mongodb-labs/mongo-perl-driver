8689c1d8e660eb28dba44ec81ef978c0c9ea9601
Florian Ragwitz <rafl@debian.org>
2009-03-07 05:20:44 +0100

    Add a basic collection class.

---
+++ lib/Mongo/Collection.pm
@@ -0,0 +1,37 @@
+package Mongo::Collection;
+
+use Mouse;
+
+has _database => (
+    is       => 'ro',
+    isa      => 'Mongo::Database',
+    required => 1,
+    handles  => [qw/query find_one insert/],
+);
+
+has name => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+around qw/query find_one insert/ => sub {
+    my ($next, $self, @args) = @_;
+    return $self->$next($self->_query_ns, @args);
+};
+
+sub _query_ns {
+    my ($self) = @_;
+    return $self->name;
+}
+
+sub count {
+    my ($self) = @_;
+    my $obj = $self->_database->run_command({ count => $self->name });
+    return $obj->{n};
+}
+
+no Mouse;
+__PACKAGE__->meta->make_immutable;
+
+1;
