e9aa2885ce7ad4ff9975cf719e149637bf4cfeca
Florian Ragwitz <rafl@debian.org>
2009-03-07 05:18:41 +0100

    Delegate query, find_one and insert to the connection, with the namespace set according to the database name.

--- lib/Mongo/Database.pm
+++ lib/Mongo/Database.pm
@@ -6,6 +6,7 @@ has _connection => (
     is       => 'ro',
     isa      => 'Mongo::Connection',
     required => 1,
+    handles  => [qw/query find_one insert/],
 );
 
 has name => (
@@ -13,6 +14,17 @@ has name => (
     isa      => 'Str',
     required => 1,
 );
+
+around qw/query find_one insert/ => sub {
+    my ($next, $self, $ns, @args) = @_;
+    return $self->$next($self->_query_ns($ns), @args);
+};
+
+sub _query_ns {
+    my ($self, $ns) = @_;
+    my $name = $self->name;
+    return qq{${name}.${ns}};
+}
 
 no Mouse;
 __PACKAGE__->meta->make_immutable;
