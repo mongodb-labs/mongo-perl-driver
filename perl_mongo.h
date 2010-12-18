622ed2706f1ad99490a0e6df1de7306bf36e0ba2
Florian Ragwitz <rafl@debian.org>
2010-12-18 18:58:53 +0100

    More portability through Devel::PPPort

--- perl_mongo.h
+++ perl_mongo.h
@@ -23,9 +23,7 @@
 #include "perl.h"
 #include "XSUB.h"
 
-#ifndef mXPUSHp
-#define mXPUSHp(p,l) STMT_START { EXTEND(sp,1); sv_setpvn(PUSHmort al, (p), (l)); } STMT_END
-#endif
+#include "ppport.h"
 
 #define PERL_MONGO_CALL_BOOT(name)  perl_mongo_call_xs (aTHX_ name, cv, mark)
 
