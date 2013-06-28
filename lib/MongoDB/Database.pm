e7c55043abf8e6c55914946547167b20126c7fcf
Mike Friedman <mike.friedman@10gen.com>
2013-06-28 11:44:44 -0400

    updating license info

--- lib/MongoDB/Database.pm
+++ lib/MongoDB/Database.pm
@@ -1,5 +1,5 @@
 #
-#  Copyright 2009 10gen, Inc.
+#  Copyright 2009-2013 10gen, Inc.
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@
 package MongoDB::Database;
 
 
-# ABSTRACT: A Mongo Database
+# ABSTRACT: A MongoDB Database
 
 use Moose;
 use MongoDB::GridFS;
