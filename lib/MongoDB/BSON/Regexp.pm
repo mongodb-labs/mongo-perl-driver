9b47a375c2231b7a47a0dd0373e4cc4eb54f2792
Mike Friedman <mike.friedman@10gen.com>
2013-12-03 15:24:24 -0400

    PERL-221 created MongoDB::BSON::Regexp class

---
+++ lib/MongoDB/BSON/Regexp.pm
@@ -0,0 +1,43 @@
+package MongoDB::BSON::Regexp;
+
+use Moose;
+
+has pattern => ( 
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+has flags => ( 
+    is        => 'ro',
+    isa       => 'Str',
+    required  => 0,
+    predicate => 'has_flags',
+    writer    => '_set_flags',
+);
+
+my %ALLOWED_FLAGS = ( 
+    i   => 1,
+    m   => 1,
+    x   => 1,
+    l   => 1,
+    s   => 1,
+    u   => 1
+);
+
+sub BUILD { 
+    my $self = shift;
+
+    if ( $self->has_flags ) { 
+        my %seen;
+        my @flags = grep { !$seen{$_}++ } split '', $self->flags;
+        foreach my $f( @flags ) { 
+            die "Regexp flag $f is not supported by MongoDB" if not exists $ALLOWED_FLAGS{$f};
+        }
+
+        $self->_set_flags( join '', @flags );
+    }
+}
+
+
+1;
