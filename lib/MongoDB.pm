435e9070910b9376b4ec6cc0fa6f562aecbaf0df
Ryan Chipman <ryan@ryanchipman.com>
2015-07-31 10:37:33 -0400

    Fix tense error
    Changes "reverts" to "will revert", since the 1.0.0 release is in the
    future, not the present. May be worth changing back to present tense
    in releases >= 1.0.0.

--- lib/MongoDB.pm
+++ lib/MongoDB.pm
@@ -241,7 +241,7 @@ constants.
 
 =head1 SEMANTIC VERSIONING SCHEME
 
-Starting with MongoDB C<v1.0.0>, the driver reverts to the more-familiar
+Starting with MongoDB C<v1.0.0>, the driver will revert to the more-familiar
 three-part, version-tuple numbering scheme used by both Perl and MongoDB:
 C<vX.Y.Z>
 
