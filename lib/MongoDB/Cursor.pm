mongodb-labs/mongo-perl-driver

commit 00ecde8d72371923558fecf960ac9f0f2b725da7

Fix role refactoring for older versions of mongod

lib/MongoDB/Cursor.pm
@@ -464,7 +464,7 @@ sub explain {
         read_concern    => $self->_query->read_concern,
     );
 
-    return $self->_query->client->send_read_op($explain_op);
+    return $self->_query->client->send_read_op($explain_op)->next;
 }
 
 =head1 QUERY ITERATION

lib/MongoDB/Op/_Explain.pm
@@ -94,7 +94,7 @@ sub _legacy_explain {
     # explain implementations, it also ensures a cursor isn't left open.
     $new_query->limit( -1 * abs( $new_query->limit ) );
 
-    return $new_query->execute->next;
+    return $new_query->execute( $link, $topology );
 }
 
 1;

lib/MongoDB/MongoClient.pm
@@ -1422,7 +1422,7 @@ sub send_write_op {
     my ( $self, $op ) = @_;
     my ( $link, $result );
     ( $link = $self->{_topology}->get_writable_link ), (
-        eval { ($result) = $op->execute($link); 1 } or do {
+        eval { ($result) = $op->execute($link, $self->{_topology}->type); 1 } or do {
             my $err = length($@) ? $@ : "caught error, but it was lost in eval unwind";
             if ( $err->$_isa("MongoDB::ConnectionError") ) {
                 $self->{_topology}->mark_server_unknown( $link->server, $err );

lib/MongoDB/Op/_FSyncUnlock.pm
@@ -26,6 +26,7 @@ use Moo;
 
 use MongoDB::Op::_Command;
 use MongoDB::Op::_Query;
+use MongoDB::ReadConcern;
 use MongoDB::ReadPreference;
 use MongoDB::_Types qw(
     Document
@@ -98,10 +99,12 @@ sub _legacy_fsync_unlock {
         sort                => undef,
         db_name             => 'admin',
         coll_name           => '$cmd.sys.unlock',
+        full_name           => 'admin.$cmd.sys.unlock',
         limit               => -1,
         bson_codec          => $self->bson_codec,
         client              => $self->client,
         read_preference     => MongoDB::ReadPreference->new,
+        read_concern        => MongoDB::ReadConcern->new,
     );
 
     return $op->execute( $link, $topology )->next;

lib/MongoDB/Op/_ListCollections.pm
@@ -26,6 +26,7 @@ use Moo;
 
 use MongoDB::Op::_Command;
 use MongoDB::Op::_Query;
+use MongoDB::ReadConcern;
 use MongoDB::ReadPreference;
 use MongoDB::_Types qw(
     Document
@@ -132,9 +133,11 @@ sub _legacy_list_colls {
         %{$self->options},
         db_name         => $self->db_name,
         coll_name       => 'system.namespaces',
+        full_name       => $self->db_name . ".system.namespaces",
         bson_codec      => $self->bson_codec,
         client          => $self->client,
         read_preference => MongoDB::ReadPreference->new,
+        read_concern    => MongoDB::ReadConcern->new,
         filter          => $self->filter,
         post_filter => \&__filter_legacy_names
     );

lib/MongoDB/Op/_ListIndexes.pm
@@ -26,6 +26,8 @@ use Moo;
 use MongoDB::Error;
 use MongoDB::Op::_Command;
 use MongoDB::Op::_Query;
+use MongoDB::ReadConcern;
+use MongoDB::ReadPreference;
 use Types::Standard qw(
     InstanceOf
 );
@@ -101,9 +103,11 @@ sub _legacy_list_indexes {
         sort                => undef,
         db_name             => $self->db_name,
         coll_name           => 'system.indexes',
+        full_name           => $self->db_name . '.system.indexes',
         bson_codec          => $self->bson_codec,
         client              => $self->client,
         read_preference     => MongoDB::ReadPreference->new,
+        read_concern        => MongoDB::ReadConcern->new,
         filter              => Tie::IxHash->new( ns => $ns ),
     );
   

lib/MongoDB/Op/_Query.pm
@@ -227,7 +227,7 @@ sub _legacy_query {
         : exists $query->{'$query'}{query}
       );
 
-    my $ns         = $self->db_name . "." . $self->coll_name;
+    my $full_name  = $self->full_name;
     my $filter     = $self->bson_codec->encode_one( $query );
 
     # rules for calculating initial batch size
@@ -246,7 +246,7 @@ sub _legacy_query {
     $self->_apply_read_prefs( $link, $topology, $query_flags, \$query);
 
     my ( $op_bson, $request_id ) =
-      MongoDB::_Protocol::write_query( $ns, $filter, $proj, $self->skip, $n_to_return,
+      MongoDB::_Protocol::write_query( $full_name, $filter, $proj, $self->skip, $n_to_return,
         $query_flags );
 
     my $result =
@@ -258,7 +258,7 @@ sub _legacy_query {
     return $class->_new(
         _client       => $self->client,
         _address      => $link->address,
-        _ns           => $ns,
+        _full_name    => $full_name,
         _bson_codec   => $self->bson_codec,
         _batch_size   => $n_to_return,
         _cursor_at    => 0,

commit 782500339d178f4a552283d30ac5b124942a7853

Fix cursor explain bug introduced in 00ecde8 on mongod 3.2

lib/MongoDB/Cursor.pm
@@ -464,7 +464,7 @@ sub explain {
         read_concern    => $self->_query->read_concern,
     );
 
-    return $self->_query->client->send_read_op($explain_op)->next;
+    return $self->_query->client->send_read_op($explain_op);
 }
 
 =head1 QUERY ITERATION

lib/MongoDB/Op/_Explain.pm
@@ -94,7 +94,7 @@ sub _legacy_explain {
     # explain implementations, it also ensures a cursor isn't left open.
     $new_query->limit( -1 * abs( $new_query->limit ) );
 
-    return $new_query->execute( $link, $topology );
+    return $new_query->execute( $link, $topology )->next;
 }
 
 1;