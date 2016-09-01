712f8018d955ccb307a4f0d554035938fb8fa661
J. Rassi <rassi@10gen.com>
2016-09-01 19:14:10 -0400

    PERL-649 Add new module _Platform
    Reports data about the running system and Perl environment.

---
+++ lib/MongoDB/_Platform.pm
@@ -0,0 +1,37 @@
+#
+#  Copyright 2016 MongoDB, Inc.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#
+
+use 5.008;
+use strict;
+use warnings;
+
+package MongoDB::_Platform;
+
+use version;
+our $VERSION = 'v1.5.0';
+
+use Config;
+
+sub os_type {
+    return ($^O eq "MSWin32") ? "Windows" : $^O;
+}
+
+sub platform_details {
+    my $perl_version = version->new($])->normal;
+    return "Perl $perl_version $Config{archname}";
+}
+
+1;
