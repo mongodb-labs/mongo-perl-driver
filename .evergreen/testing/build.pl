8975bddf0c4637b97b09cf12d270ecae8fb63c16
David Golden <xdg@xdg.me>
2017-01-25 16:35:23 -0500

    CI: revise build/test mechanism to use new config generator

--- .evergreen/testing/build.pl
+++ .evergreen/testing/build.pl
@@ -10,7 +10,7 @@ use EvergreenHelper;
 # Bootstrap
 bootstrap_env();
 
-run_in_dir 'mongo-perl-driver' => sub {
+run_in_dir $ENV{REPO_DIR} => sub {
     # Install any new, missing dependencies to local library
     run_local_cpanm(qw/--installdeps ./);
 
@@ -18,6 +18,6 @@ run_in_dir 'mongo-perl-driver' => sub {
     configure();
     make();
 
-    # Archive both built blib and local deps for reuse
-    try_system(qw/tar -czf build.tar.gz blib local/);
+    # Archive local deps for reuse
+    try_system(qw/tar -czf build.tar.gz local/);
 };
--- .evergreen/testing/test.pl
+++ .evergreen/testing/test.pl
@@ -10,15 +10,15 @@ use EvergreenHelper;
 # Bootstrap
 bootstrap_env();
 
-run_in_dir 'mongo-perl-driver' => sub {
+run_in_dir $ENV{REPO_DIR} => sub {
     # Configure ENV vars for local library updated in compile step
     bootstrap_locallib('local');
 
-    # Test with and without asserts
-    for my $n ( 0, 1 ) {
-        print "Testing with PERL_MONGO_WITH_ASSERTS = $n\n";
-        $ENV{PERL_MONGO_WITH_ASSERTS} = $n;
-        make("test");
-    }
+    # Configure & build (repeated to regenerate all object files)
+    configure();
+    make();
+
+    # Run tests
+    make("test");
 };
 
