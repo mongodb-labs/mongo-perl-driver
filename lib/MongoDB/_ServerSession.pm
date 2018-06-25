7afe9e91f9d45bd84105cb9ae5af89a0e21c37f8
David Golden <xdg@xdg.me>
2018-06-25 10:31:34 -0400

    minor: remove POD for private _ServerSession class

--- lib/MongoDB/_ServerSession.pm
+++ lib/MongoDB/_ServerSession.pm
@@ -16,7 +16,7 @@ use strict;
 use warnings;
 package MongoDB::_ServerSession;
 
-# ABSTRACT: MongoDB Server Session object
+# MongoDB Server Session object
 
 use version;
 our $VERSION = 'v1.999.2';
@@ -38,16 +38,14 @@ use constant UUID_TYPE => 4;
 
 use namespace::clean -except => 'meta';
 
-=attr session_id
-
-    $server_session->session_id;
-
-Returns the session id for this server session as a L<BSON::Bytes> object
-containing a binary UUID V4. For lower network usage, if not provided on
-initialisation this class will generate a new UUID instead of consulting the
-server for a new session id.
-
-=cut
+# session_id
+#
+#     $server_session->session_id;
+#
+# Returns the session id for this server session as a L<BSON::Bytes> object
+# containing a binary UUID V4. For lower network usage, if not provided on
+# initialisation this class will generate a new UUID instead of consulting the
+# server for a new session id.
 
 has session_id => (
     is => 'lazy',
@@ -64,15 +62,13 @@ sub _build_session_id {
     return { id => $uuid };
 }
 
-=attr last_use
-
-    $server_session->last_use;
-
-Returns the unix time that this server session was last used. Used for checking
-expiry of a server session. If undefined, then the session has (probably) not
-been used on the server.
-
-=cut
+# last_use
+#
+#     $server_session->last_use;
+#
+# Returns the unix time that this server session was last used. Used for checking
+# expiry of a server session. If undefined, then the session has (probably) not
+# been used on the server.
 
 has last_use => (
     is => 'rwp',
@@ -80,15 +76,13 @@ has last_use => (
     isa => Maybe[Int],
 );
 
-=attr transaction_id
-
-    $server_session->transaction_id
-
-Returns the current transaction id for this server session. This is a ratcheted
-incrementing ID number, which when combined with the session id allows for
-retrying transactions in the correct order.
-
-=cut
+# transaction_id
+#
+#     $server_session->transaction_id
+#
+# Returns the current transaction id for this server session. This is a ratcheted
+# incrementing ID number, which when combined with the session id allows for
+# retrying transactions in the correct order.
 
 has transaction_id => (
     is => 'rwp',
@@ -96,13 +90,11 @@ has transaction_id => (
     default => sub { Math::BigInt->new('0') },
 );
 
-=method update_last_use
-
-    $server_session->update_last_use;
-
-Updates the value of L</last_use> to the current unix time.
-
-=cut
+# update_last_use
+#
+#     $server_session->update_last_use;
+#
+# Updates the value of L</last_use> to the current unix time.
 
 sub update_last_use {
     my ( $self ) = @_;
@@ -125,19 +117,4 @@ sub _is_expiring {
 
 1;
 
-__END__
-
-=pod
-
-=head1 SYNOPSIS
-
-    use MongoDB::_ServerSession;
-
-    my $server_session = MongoDB::_ServerSession->new;
-
-=head1 DESCRIPTION
-
-This class encapsulates the session id and last use of the session. For use
-with L<MongoDB::ClientSession> for session based operations.
-
-=cut
+# vim: set ts=4 sts=4 sw=4 et tw=75:
